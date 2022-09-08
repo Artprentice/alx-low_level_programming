7487a87bf255024b0b3f9eefab1bf75a4f2a5d15
abdullah <abdallahsuleiman56@gmail.com>
2022-09-08 07:06:41 -0700

    c program to print with puts()

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	/*Initialiizing string variable*/
+	char string[] = "Programming is like building a multilingual puzzle";
+	/*Using puts() to print to output*/
+	puts("string");
+
+	return 0;
+}
