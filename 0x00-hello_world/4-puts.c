10c9592c6647d6ee2de91a97fad0041857c33e09
abdullah <abdallahsuleiman56@gmail.com>
2022-09-08 07:24:23 -0700

    qakale

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,20 @@
 #include<stdio.h>
+/** 
+ * main - puts to output
+ * @void: only param
+ * 
+ * Return:0
+ */
 int main(void)
 {	
-	/*print to output using puts()*/
-	char string[] = "Programming is like building a multilingual puzzle";
+	/**
+	 * puts - print to output using puts()
+	 * @string - variable holding string
+	 * Return : 0 as required
+	 */
+	char string[] = "\"Programming is like building a multilingual puzzle";
+	
 	puts(string);
+	
 	return 0;
 }
