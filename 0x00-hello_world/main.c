e4838fa822eab3b51534186555d2df9e514fab25
abdullah <abdallahsuleiman56@gmail.com>
2022-09-08 09:37:53 -0700

    it self explanatory comem  on

---
+++ 0x00-hello_world/main.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - main func
+ * @void: default param
+ * Return: default
+ */
+int main(void)
+{
+	/**
+	 * printf - prints to output
+	 * @parameters: string to printed
+	 * Return: 0
+	 */
+	printf("Random String");
+
+	return (0);
+}
