7da7304fee1391d5b8b7ff9604b0c02f33b4f247
abdullah <abdallahsuleiman56@gmail.com>
2022-09-08 07:26:17 -0700

    lets see now

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,16 +5,13 @@
  * 
  * Return:0
  */
-int main(void)
-{	
+int main(void){	
 	/**
 	 * puts - print to output using puts()
 	 * @string - variable holding string
 	 * Return : 0 as required
 	 */
 	char string[] = "\"Programming is like building a multilingual puzzle";
-	
 	puts(string);
-	
-	return 0;
+	return();
 }
