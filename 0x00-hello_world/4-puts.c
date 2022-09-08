7cc462009e45990ebcf76f6aa05ebaa3899eb99b
abdullah <abdallahsuleiman56@gmail.com>
2022-09-08 07:33:30 -0700

    whitespace removed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,18 @@
 #include<stdio.h>
-/** 
+/**
  * main - puts to output
  * @void: only param
- * 
+ *
  * Return:0
  */
-int main(void){	
+int main(void){
 	/**
 	 * puts - print to output using puts()
 	 * @string - variable holding string
 	 * Return : 0 as required
 	 */
 	char string[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(string);
 	return 0;
 }
