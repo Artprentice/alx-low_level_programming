cc29588b98f954919d531fec33dcbd7b9f2029a0
abdullah <abdallahsuleiman56@gmail.com>
2022-09-08 08:27:03 -0700

    printing to output with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - main func
+ * @void: Default param
+ * Return: 0
+ */
+int main(void)
+{
+	/**
+	 * printf - Print to output
+	 * @string: String to be printed using printf
+	 * Return: 0
+	 */
+	char string[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf(string);
+	return (0);
+}
