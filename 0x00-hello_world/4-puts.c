f8b98798033280aaac9c5b12d35c652359e2e541
abdullah <abdallahsuleiman56@gmail.com>
2022-09-08 07:13:34 -0700

    same

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,7 @@
 #include<stdio.h>
 int main()
 {
-	/*Initialiizing string variable*/
 	char string[] = "Programming is like building a multilingual puzzle";
-	/*Using puts() to print to output*/
-	puts("string");
-
+	puts(string);
 	return 0;
 }
