8b51fb767e95afb81e891043c7ac5c757b420d06
abdullah <abdallahsuleiman56@gmail.com>
2022-09-09 12:39:33 -0700

    s

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,16 +11,21 @@ int main(void)
 	 * @a: var
 	 * Return: 0
 	 */
-	int a, b;
+	int a;
+	
+	int b;
 
-	for (b = a + 1; b < 10; b++)
+	for (a = 0; a < 9; a++)
 	{
+		for (b = a + 1; b < 10; b++)
+		{
 		putchar((a % 10) + '0');
 		putchar((b % 10) + '0');
 		if (a == 8 && b == 9)
 			continue;
 		putchar(',');
 		putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
