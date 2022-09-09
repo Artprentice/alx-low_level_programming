10f839f29fc38ff0873f9a864f34e2314ceb958a
abdullah <abdallahsuleiman56@gmail.com>
2022-09-09 09:56:52 -0700

    betty

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,19 +3,19 @@
 /**
  * main - entry pt
  * @void:default param
- * return: always 0
+ * Return: always 0
  */
 int main(void)
 {
 	/**
 	 * for - loop
 	 * @dig: variable
-	 * return: 0
+	 *Return: 0
 	 */
 	int dig;
 
 	for (dig = 0; dig <= 9; dig++)
-		printf("%d",dig);
+		printf("%d", dig);
 
 	printf("\n");
 	return (0);
