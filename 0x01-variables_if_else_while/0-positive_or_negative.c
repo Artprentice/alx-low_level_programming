d26b0502d5744157e110bf7e6e24b574e28ba71f
abdullah <abdallahsuleiman56@gmail.com>
2022-09-09 06:45:41 -0700

    check if no in positive negative or zero

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - main func
+ * @void: default param
+ */
+int main(void)
+{
+	/**
+	 * if - conditional
+	 * @n: number to be checked
+	 * Return: 0
+	 */
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}else
+	{
+		printf("%d is negative\n", n);
+	}
+	return(0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+This is me learning conditionals and loops in C
