f11800443295bc98a0f2463086f74501dbb2c6b6
abdullah <abdallahsuleiman56@gmail.com>
2022-09-09 09:18:02 -0700

    print alphabets in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main func
+ * @void: default param
+ * Return: always
+ */
+int main(void)
+{
+	/**
+	 * while - loop
+	 * @charr: variable
+	 * Return: always 0
+	 */
+	int charr = 'a';
+	
+	while (char <= 'z')
+	{
+		putchar(charr);
+		a++;
+	}
+	
+	return (0);
+}
