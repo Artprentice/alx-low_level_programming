ed11a2b32885cc098ffaae150579167cae5ed3c8
abdullah <abdallahsuleiman56@gmail.com>
2022-09-09 10:01:51 -0700

    same

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry pt
+ * @void: default
+ * Return: always
+ */
+int main(void)
+{
+	/**
+	 * for - loop
+	 * @dig: var
+	 * Return: 0
+	 */
+	int dig;
+	
+	for (dig = 0; dig <= 9; dig++)
+		putchar(dig);
+
+	putchar('\n');
+}
