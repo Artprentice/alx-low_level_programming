9b0342c6e29fb542c975fa7e9a0a3c5a6cd9c2ba
abdullah <abdallahsuleiman56@gmail.com>
2022-09-09 10:22:02 -0700

    s

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - entry
+ * @void: param
+ * Return: 0
+ */
+int main(void)
+{
+	/**
+	 * for - loop
+	 * @dig:var
+	 * @charr:var
+	 * Return: 0
+	 */
+	int dig;
+
+	char charr;
+
+	for (dig = 1; dig <= 9; dig++)
+		putchar((dig % 10) + '0')
+
+	for (charr = 'a'; charr <= 'f'; charr++)
+		putchar(charr);
+
+	putchar('\n');
+	return (0);
+}
