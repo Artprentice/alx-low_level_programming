71c7f28fa705a7fc9a86b329258c6dc0f24f2248
abdullah <abdallahsuleiman56@gmail.com>
2022-09-09 11:28:33 -0700

    s

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,15 +14,16 @@ int main(void)
 	 */
 	int a;
 
-	int b;
-
-	for (a = 0; a <= 9; a++);
+	for (a = 0; a <= 9; a++)
 	{
-		for (b = 0; b <= 9; b++)
+		putchar(x + '0');
+		if (x <= 9)
 		{
-			putchar(((a % 10) + '0' + ',') + (b % 10) + '0');
+			putchar(',');
+			putchar(' ');
 		}
-		putchar('\n');
-		return (0);
+	}
+	putchar('\n');
+	return (0);
 	}
 }
