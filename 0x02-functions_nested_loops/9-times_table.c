4dcb1789d69c4d6a9ea1ebdeebb7849b9c69c5ed
abdullah <abdallahsuleiman56@gmail.com>
2022-09-13 11:53:55 -0700

    a

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,6 +7,7 @@
 void times_table(void)
 {
 	int num, mult, prod;
+
 	for (num = 0; num <= 9; num++)
 	{
 		_putchar('0');
@@ -20,7 +21,6 @@ void times_table(void)
 				_putchar(' ');
 			else
 				_putchar((prod / 10) + '0');
-				
 			_putchar((prod % 10) + '0');
 		}
 		_putchar('\n');
