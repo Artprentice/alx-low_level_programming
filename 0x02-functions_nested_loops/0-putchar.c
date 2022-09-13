b1ed1b80882f264bacacd69a730f2ca599ed5fa3
abdullah <abdallahsuleiman56@gmail.com>
2022-09-13 05:34:17 -0700

    a

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry
+ * Return: Always
+ */
+int main(void)
+{
+	/**
+	 * printf - prints to screen
+	 * Return: 0
+	 */
+	printf("_putchar\n");
+	return (0);
+}
+
