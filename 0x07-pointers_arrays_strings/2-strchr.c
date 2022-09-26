b60c4f492cd7a8228be940c41ce9139935552fb8
Abdullah <abdallahsuleiman56@gmail.com>
2022-09-26 21:08:34 +0100

    a

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -3,7 +3,6 @@
  * _strchr - Locates a character in a string.
  * @s: The string to be searched.
  * @c: The character to be located.
- *
  * Return: If c is found - a pointer to the first occurence.
  * If c is not found - NULL.
  */
@@ -17,4 +16,4 @@ char *_strchr(char *s, char c)
 			return (s + i);
 	}
 	return ('\0');
-}
+	}
