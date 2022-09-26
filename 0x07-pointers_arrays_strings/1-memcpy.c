667941765bdf1f75ccaf3e413c086c6d0618f8b0
Abdullah <abdallahsuleiman56@gmail.com>
2022-09-26 18:09:01 +0100

    a

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,8 +9,8 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
-	*destination = dest;
-	*source = src;
+	char *destination = dest;
+	char *source = src;
 
 	for (i = 0; i < n; i++)
 		destination[i] = source[i];
