344e89b3c5a0857156996e5a96caa14a739eb9a6
Abdullah <abdallahsuleiman56@gmail.com>
2022-09-28 19:57:19 +0100

    a

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -17,11 +17,10 @@ int is_divi(int num, int div)
 	return (is_divi(num, div + 1));
 }
 /**
- * is_prime_number - Checks if a number is prime.
- * @n: The number to be checked.
- *
- * Return: If the integer is not prime = 0
- *         If the number is prime = 1
+ * _sqrt_recursion - Returns the natural square root of a number
+ * @n: The number to return the square root of
+ * Return: If n has a natural square root - the natural square root of n.
+ *         If n does not have a natural square root - -1.
  */
 int is_prime_number(int n)
 {
