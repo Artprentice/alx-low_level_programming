a57cca6bbceb5b7a8e10681b84aa1710b6bd6190
Abdullah <abdallahsuleiman56@gmail.com>
2022-10-06 14:37:46 +0100

    a

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,7 +9,7 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	unsigned int i, t;
-	void *p;
+	char *p;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
@@ -21,5 +21,5 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	for(i = 0; i < (t); i++)
 		p[i] = 0;
-	return (p);
+	return ((void *)p);
 }
