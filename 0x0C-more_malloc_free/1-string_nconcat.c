13c2caf306c360b43ac04150bf183d7c7be2cf40
Abdullah <abdallahsuleiman56@gmail.com>
2022-10-08 13:38:52 +0100

    a

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,28 +2,40 @@
 #include <stdlib.h>
 
 /**
-* _calloc - allocates memory for an array initialized it to zero
-* @nmemb: num of elements in the array
-* @size: size of the array
-* Return: returns a pointer to the allocated memory
+* string_nconcat - a function that concatenates two strings
+* @s1: the first string
+* @s2: the second string
+* @n: the number of bytes of s2 to be concatenated
+* Return: returns a pointer to a new string which contains
+* s1 followed by the first n bytes of s2, and a=the null-terminator
 */
-void *_calloc(unsigned int nmemb, unsigned int size)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, t;
+	unsigned int i, j;
+	unsigned int c1 = 0, c2 = 0, c = 0;
+	char *str;
 
-	char *p;
+	if (s1 != NULL)
+		for (i = 0; s1[i] != '\0'; i++)
+			c1++;
 
-	if (nmemb == 0 || size == 0)
+	if (s2 != NULL)
+		for (i = 0; s2[i] != '\0'; i++)
+			if (i < n)
+				c2++;
+
+	c = c1 + c2 + 1;
+
+	str = malloc(c);
+	if (str == NULL)
 		return (NULL);
 
-	t = nmemb * size;
+	for (i = 0; i < c1; i++)
+		str[i] = s1[i];
 
-	p = malloc(t);
-	if (p == NULL)
-		return (NULL);
+	for (j = 0 ; j < c2; i++, j++)
+		str[i] = s2[j];
+	str[i] = '\0';
 
-	for (i = 0; i < t; i++)
-		p[i] = 0;
-
-	return ((void *) p);
+	return (str);
 }
