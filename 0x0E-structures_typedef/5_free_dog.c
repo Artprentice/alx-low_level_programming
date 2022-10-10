a0f7c4e4783fdc31a5ccb8f39eca60909c773f57
Artprentice <111006528+Artprentice@users.noreply.github.com>
2022-10-10 18:21:48 +0100

    Update 5_free_dog.c

--- 0x0E-structures_typedef/5_free_dog.c
+++ 0x0E-structures_typedef/5_free_dog.c
@@ -1,16 +1,16 @@
 #include "dog.h"
 #include <stdlib.h>
+
 /**
- * free_dog - frees dogs.
- * @d: pointer to a dog of type dog_t
- * Return: nothing
- **/
+ * free_dog - Frees dogs.
+ * @d: The dog to be freed.
+ */
 void free_dog(dog_t *d)
 {
-	if (d != NULL)
+	if (d == NULL)
 		return;
 
-	free(d->name);
 	free(d->owner);
+	free(d->name);
 	free(d);
 }
