b452253220e1173d143d09ff2b6a5e2add7186f2
Abdullah <abdallahsuleiman56@gmail.com>
2022-10-24 20:36:30 +0100

    fix

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,13 +1,18 @@
-#include "list.h"
+#include "lists.h"
+
 /**
- * insert_nodeint_at_index - inserts new node at index
- * @head: pointer to address of head of listint_t list
- * @idx: index where new node is wanted
- * @n: the data the new node should contain
- * Return: if failed - NULL otherwise - address
- *	of new node
+ * insert_nodeint_at_index - Inserts a new node to a listint_t
+ *                           list at a given position.
+ * @head: A pointer to the address of the
+ *        head of the listint_t list.
+ * @idx: The index of the listint_t list where the new
+ *       node should be added - indices start at 0.
+ * @n: The integer for the new node to contain.
+ *
+ * Return: If the function fails - NULL.
+ *         Otherwise - the address of the new node.
  */
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new, *copy = *head;
 	unsigned int node;
@@ -15,6 +20,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
+
 	new->n = n;
 
 	if (idx == 0)
